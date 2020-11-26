8dc174ac3ab8156fa75af3c200bb1ef663f8ded3
Erjon <erjonlusha@gmail.com>
2020-11-26 17:14:07 +0100

    Hamburger menu jQuery
    Hamburger menu:
    mostrare / nascondere il menu principale

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+$("div.header-right > a").click(function () { 
+  $(".hamburger-menu").show();
+});
+
+
+
+
+$(".hamburger-menu >a.close").click(function () { 
+  $(".hamburger-menu").hide();
+});
